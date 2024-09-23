ee5ef37509cbf7fb82b660416f9fd0d9740efbe4
Ted <shinsuengju1@gmail.com>
2024-09-24 00:01:08 +0900

    f

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,7 @@
 //# Json-Server API_URL (MOCK DATA)
 export const LOCALHOST_PORT =
   process.env.REACT_APP_API_URL || 'http://localhost:3001';
+
+if (!process.env.REACT_APP_API_URL) {
+  console.warn('Using default API URL:', LOCALHOST_PORT);
+}
