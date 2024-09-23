8a2bc8aaf12769d57e1725e3180e0236097cd18f
Ted <shinsuengju1@gmail.com>
2024-09-23 22:43:05 +0900

    서버배포테스트

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,3 @@
 //# Json-Server API_URL (MOCK DATA)
-export const LOCALHOST_PORT = 'http://localhost:3001';
+// export const LOCALHOST_PORT = 'http://localhost:3001';
+export const LOCALHOST_PORT = 'https://jays-toy-store.onrender.com/';
