60f305d55e114ed269e142827ba7e3bae9e3e84e
Ted <shinsuengju1@gmail.com>
2024-09-24 12:27:56 +0900

    해쉬라우터사용

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <App />
       </HashRouter>
     </ThemeProvider>
